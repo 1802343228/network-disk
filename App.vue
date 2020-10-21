dfc0453dd2a2eddeb7c56b11e46d0562bf6a1d44
crq <2631315464@qq.com>
2020-10-21 09:47:10 +0800

    封装自定义导航栏，使用solt来传递自定义导航左中右的内容，并固定其位置

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 引入官方样式库 */
+	@import url("/common/uni.css");
+	/* 引入自定义图标 */
+	@import url("./common/icon.css");
+	/* 自定义样式库 */
+	@import url("./common/free.css");
+	/* 引入公共样式 */
+	@import url("./common/common.css");
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<nav-bar>
+			<text slot="left" class="font-md ml-3">首页</text>
+			<template slot="right">
+				<view style="width: 60rpx;height: 60rpx;"
+				 class="flex align-center justify-center bg-light rounded-circle mr-3">
+					<text class="iconfont icon-zengjia"></text>
+				</view>
+				<view style="width: 60tpx;height: 60rpx;"
+				 class="flex align-center justify-center bg-light rounded-circle mr-3">
+					<text class="iconfont icon-gengduo"></text>
+				</view>
+			</template>
+		</nav-bar>
+		<view class="bg-info" style="height: 1000px;">123</view>
+	</view>
+</template>
+
+<script>
+	import navBar from '../../components/common/nav-bar.vue';
+	export default {
+		components: {
+			navBar
+		},
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
