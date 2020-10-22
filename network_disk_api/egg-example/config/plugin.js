f809295f062cfa2612d6c0a6b374c35d92d6b914
crq <2631315464@qq.com>
2020-10-22 10:15:54 +0800

    egg.js全局相关配置，连接创建数据库

--- network_disk_api/egg-example/config/plugin.js
+++ network_disk_api/egg-example/config/plugin.js
@@ -5,7 +5,11 @@ module.exports = {
   cors:{
     enable: true,
   package: 'egg-cors',
-  }
+  },
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize',
+  },
   // had enabled by egg
   // static: {
   //   enable: true,
